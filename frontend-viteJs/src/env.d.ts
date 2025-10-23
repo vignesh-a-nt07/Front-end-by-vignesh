e7a748287907d981193e4e61cb943ac72039eb0e
Vignesh Arumugam <vignesh.a@navattech.com>
2025-10-23 15:31:53 +0530

    Delete frontend-viteJs directory

--- frontend-viteJs/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-  VITE_BACKEND_HOST: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-/// <reference types="vite/client" />
--- frontend-viteJs/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  VITE_BACKEND_HOST: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
