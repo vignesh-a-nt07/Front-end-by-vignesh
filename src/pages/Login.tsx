f187b3019a2dd3b5b7c80e29904b2de691e327f1
vignesh-a-nt07 <vignesh.a@navattech.com>
2025-10-29 11:46:01 +0530

    added abi

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -87,7 +87,7 @@ const Login: React.FC = () => {
             sx={{ width: '100%', bgcolor: 'background.paper', p: 4, borderRadius: 2, boxShadow: 3 }}
           >
             <Typography variant="h5" component="h1" gutterBottom>
-              HariHub🔥 ATS Login
+              AbiHub🔥 ATS Login
             </Typography>
 
             {error && (
